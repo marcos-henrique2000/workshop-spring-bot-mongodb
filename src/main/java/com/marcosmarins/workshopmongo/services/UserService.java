a5d7fd76755f43a30c7af99e87470b0e3fc53330
Marcos Marins <marcos-henrique2000@hotmail.com>
2020-02-07 16:28:32 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcosmarins/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.marcosmarins.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcosmarins.workshopmongo.domain.User;
+import com.marcosmarins.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+}
